e6a2e5264b8ba530795d764b19320c6d8fd9e1e8
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2024-07-23 17:37:47 -0400

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -10,14 +10,17 @@ def extract(root, case, start, end):
         -i {root}/videos/{case}.mp4 \
         -vf "fps=1" \
         {root}/frames/{case}/frame_%04d.jpg'
-    os.system(cmd)
+  
+  os.system(cmd)
 
 if __name__ == "__main__":
   root = "/data/shared/cataract-1K/phase_recognition"
   cases = os.listdir(f"{root}/annotations")
+  df = pd.DataFrame(columns=["frame_path", "phase"])
   
   for c in cases:
     f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
     for _f in f:
       start, end = math.ceil(_f["sec"]), math.ceil(_f["endSec"])
+      cmts = _f["comments"]
       extract(root, c, start, end)
