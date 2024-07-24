2937b80a64b8e3e21e8f808e69c90d40507794f8
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2024-07-24 11:57:30 -0400

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -23,7 +23,7 @@ def extract_frames(root, case, start, end, category):
     # Create the ffmpeg command
     cmd = f"ffmpeg -ss {start_hms} -to {end_hms} \
             -i {root}/videos/{case}.mp4 \
-            -vf 'fps=1' {frames_dir}/frame_%04d_{start}_{end}.jpg"
+            -vf 'fps=1' {frames_dir}/frame_%04d_{start}_{end}_{category}.jpg"
     
     # Execute the command
     os.system(cmd)
@@ -32,36 +32,36 @@ if __name__ == "__main__":
     root = "/data/shared/cataract-1K/phase_recognition"
     cases = os.listdir(f"{root}/annotations")[:3]
     
-    # for c in cases:
-    #     if c != "SYNAPSE_METADATA_MANIFEST.tsv":
-    #         f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
-    #         for _, row in f.iterrows():
-    #             start, end = math.ceil(row["sec"]), math.ceil(row["endSec"])
-    #             category = row["comment"]
-    #             extract_frames(root, c, start, end, category)
+    for c in cases:
+        if c != "SYNAPSE_METADATA_MANIFEST.tsv":
+            f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
+            for _, row in f.iterrows():
+                start, end = math.ceil(row["sec"]), math.ceil(row["endSec"])
+                category = row["comment"]
+                extract_frames(root, c, start, end, category)
 
-    imgs = []
-    phases = []
-    for case in tqdm.tqdm(os.listdir(f"{root}/frames")):
-        f = pd.read_csv(f"{root}/annotations/{case}/{case}_annotations_phases.csv")
-        for img in os.listdir(f"{root}/frames/{case}"):
-            imgs.append(f"{root}/frames/{case}/{img}")
+    # imgs = []
+    # phases = []
+    # for case in tqdm.tqdm(os.listdir(f"{root}/frames")):
+    #     f = pd.read_csv(f"{root}/annotations/{case}/{case}_annotations_phases.csv")
+    #     for img in os.listdir(f"{root}/frames/{case}"):
+    #         imgs.append(f"{root}/frames/{case}/{img}")
             
-            # timing of the frame
-            _, _, ss, tt = img.split(".")[0].split("_")
-            print(ss,tt)
-            for _,i in f.iterrows():
-                start, end = math.ceil(i["sec"]), math.ceil(i["endSec"])
-                print(start, end)
-                if ss == start and tt == end:
-                    phases.append(i["comment"])
-                    break
+    #         # timing of the frame
+    #         _, _, ss, tt = img.split(".")[0].split("_")
+    #         print(ss,tt)
+    #         for _,i in f.iterrows():
+    #             start, end = math.ceil(i["sec"]), math.ceil(i["endSec"])
+    #             print(start, end)
+    #             if ss == start and tt == end:
+    #                 phases.append(i["comment"])
+    #                 break
 
-    # Convert list of dictionaries to DataFrame
-    df = pd.DataFrame({
-        "images":imgs,
-        "phases":phases
-    }, columns=["images","phases"])
+    # # Convert list of dictionaries to DataFrame
+    # df = pd.DataFrame({
+    #     "images":imgs,
+    #     "phases":phases
+    # }, columns=["images","phases"])
     
-    # Save the DataFrame to a CSV file if needed
-    df.to_csv(f"{root}/extracted_frames_with_phases.csv", index=False)
+    # # Save the DataFrame to a CSV file if needed
+    # df.to_csv(f"{root}/extracted_frames_with_phases.csv", index=False)
