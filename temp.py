70ddde33f154e367534b8f165e8c6866ae5c4cef
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2024-07-23 22:37:52 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,10 +5,10 @@ root = "/data/shared/cataract-1K/phase_recognition"
 cases = os.listdir(f"{root}/annotations")
 all_frames = []
 
-c=0
+cn=0
 for c in cases:
     if c != "SYNAPSE_METADATA_MANIFEST.tsv":
         f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
         for _, row in f.iterrows():
-            c+=1
-print(c)
+            cn+=1
+print(cn)
