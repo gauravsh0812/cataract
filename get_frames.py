45300553103a48ec72e301f05c6d76f758d3cb82
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2024-07-24 00:19:29 -0400

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -8,7 +8,7 @@ def seconds_to_hms(seconds):
     seconds = seconds % 60
     return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
 
-def extract(root, case, start, end, category):
+def extract_frames(root, case, start, end, category):
     # Convert start and end times to HH:MM:SS format
     start_hms = seconds_to_hms(start)
     end_hms = seconds_to_hms(end)
@@ -27,35 +27,42 @@ def extract(root, case, start, end, category):
     # Execute the command
     os.system(cmd)
     
-    # # List all frames generated during this extraction
-    # frames = sorted([f for f in os.listdir(frames_dir) if f.startswith('frame_')])
-    
-    # # Get the corresponding frames within the time range and associate them with the category
-    # extracted_frames = []
-    # for frame in frames:
-    #     frame_path = os.path.join(frames_dir, frame)
-    #     extracted_frames.append({"start": start_hms, "end": end_hms,
-    #                              "frame_path": frame_path, "phase": category})
-    
-    # return extracted_frames
-
 if __name__ == "__main__":
     root = "/data/shared/cataract-1K/phase_recognition"
     cases = os.listdir(f"{root}/annotations")
-    # all_frames = []
+
+    tmp = dict()
     
     for c in cases:
+        tmp[c] = []
         if c != "SYNAPSE_METADATA_MANIFEST.tsv":
             f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
             for _, row in f.iterrows():
                 start, end = math.ceil(row["sec"]), math.ceil(row["endSec"])
                 category = row["comment"]
-                # extracted_frames = extract(root, c, start, end, category)
-                extract(root, c, start, end, category)
-                # all_frames.extend(extracted_frames)
+                tmp[c].append([start,end,category])
+                extract_frames(root, c, start, end, category)
+
+    imgs = []
+    phases = []
+    for case in os.listdir(f"{root}/frames"):
+        for img in os.listdir(case):
+            imgs.append(f"{root}/frames/{case}/{img}")
+
+            # timing of the frame
+            _, _, ss, tt = img.split(".")[0].split("_")
+
+            # get the phase
+            for l in tmp[case]:
+                s, t, ctgry = l
+                if s == ss and t == tt:
+                    phases.append(ctgry)
     
     # Convert list of dictionaries to DataFrame
-    # df = pd.DataFrame(all_frames)
+    df = pd.DataFrame({
+        "images":imgs,
+        "phases":phases
+    }, columns=["images","phases"])
     
     # Save the DataFrame to a CSV file if needed
     # df.to_csv(f"{root}/extracted_frames_with_phases.csv", index=False)
