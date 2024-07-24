c17ac3fc5a4dae8f2fec78ba7ee119c458d56c44
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2024-07-23 23:25:16 -0400

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -20,27 +20,28 @@ def extract(root, case, start, end, category):
     os.makedirs(frames_dir, exist_ok=True)
 
     # Create the ffmpeg command
-    cmd = f'ffmpeg -ss {start_hms} -to {end_hms} -i {root}/videos/{case}.mp4 -vf "fps=1" {frames_dir}/frame_%04d.jpg'
+    cmd = f'ffmpeg -ss {start_hms} -to {end_hms} -i {root}/videos/{case}.mp4 -v \
+            "fps=1" {frames_dir}/frame_%04d_{start_hms}_{end_hms}.jpg'
     
     # Execute the command
     os.system(cmd)
     
-    # List all frames generated during this extraction
-    frames = sorted([f for f in os.listdir(frames_dir) if f.startswith('frame_')])
+    # # List all frames generated during this extraction
+    # frames = sorted([f for f in os.listdir(frames_dir) if f.startswith('frame_')])
     
-    # Get the corresponding frames within the time range and associate them with the category
-    extracted_frames = []
-    for frame in frames:
-        frame_path = os.path.join(frames_dir, frame)
-        extracted_frames.append({"start": start_hms, "end": end_hms,
-                                 "frame_path": frame_path, "phase": category})
+    # # Get the corresponding frames within the time range and associate them with the category
+    # extracted_frames = []
+    # for frame in frames:
+    #     frame_path = os.path.join(frames_dir, frame)
+    #     extracted_frames.append({"start": start_hms, "end": end_hms,
+    #                              "frame_path": frame_path, "phase": category})
     
-    return extracted_frames
+    # return extracted_frames
 
 if __name__ == "__main__":
     root = "/data/shared/cataract-1K/phase_recognition"
     cases = os.listdir(f"{root}/annotations")
-    all_frames = []
+    # all_frames = []
     
     for c in cases:
         if c != "SYNAPSE_METADATA_MANIFEST.tsv":
@@ -48,11 +49,12 @@ if __name__ == "__main__":
             for _, row in f.iterrows():
                 start, end = math.ceil(row["sec"]), math.ceil(row["endSec"])
                 category = row["comment"]
-                extracted_frames = extract(root, c, start, end, category)
-                all_frames.extend(extracted_frames)
+                # extracted_frames = extract(root, c, start, end, category)
+                extract(root, c, start, end, category)
+                # all_frames.extend(extracted_frames)
     
     # Convert list of dictionaries to DataFrame
-    df = pd.DataFrame(all_frames)
+    # df = pd.DataFrame(all_frames)
     
     # Save the DataFrame to a CSV file if needed
-    df.to_csv(f"{root}/extracted_frames_with_phases.csv", index=False)
+    # df.to_csv(f"{root}/extracted_frames_with_phases.csv", index=False)
