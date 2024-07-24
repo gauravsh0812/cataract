f308eae08572eb86250ccb169de2f25b2147eba3
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2024-07-24 00:56:25 -0400

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -31,35 +31,31 @@ def extract_frames(root, case, start, end, category):
 if __name__ == "__main__":
     root = "/data/shared/cataract-1K/phase_recognition"
     cases = os.listdir(f"{root}/annotations")[:3]
-
-    tmp = dict()
     
     for c in cases:
-        tmp[c] = []
         if c != "SYNAPSE_METADATA_MANIFEST.tsv":
             f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
             for _, row in f.iterrows():
                 start, end = math.ceil(row["sec"]), math.ceil(row["endSec"])
                 category = row["comment"]
-                tmp[c].append([start,end,category])
                 extract_frames(root, c, start, end, category)
 
     imgs = []
     phases = []
     for case in tqdm.tqdm(os.listdir(f"{root}/frames")):
+        f = pd.read_csv(f"{root}/annotations/{c}/{c}_annotations_phases.csv")
         for img in os.listdir(f"{root}/frames/{case}"):
             imgs.append(f"{root}/frames/{case}/{img}")
-
+            
             # timing of the frame
             _, _, ss, tt = img.split(".")[0].split("_")
-
-            # get the phase
-            print(tmp[case])
-            for l in tmp[case]:
-                s, t, ctgry = l
-                if s == ss and t == tt:
-                    phases.append(ctgry)
-    
+            
+            for _,i in f.iterrows():
+                start, end = math.ceil(row["sec"]), math.ceil(row["endSec"])
+                if ss == start and tt == end:
+                    phases.append(i["comment"])
+                    break
+            
     # Convert list of dictionaries to DataFrame
     df = pd.DataFrame({
         "images":imgs,
