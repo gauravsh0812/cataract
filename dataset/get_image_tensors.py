3c306bd4e3158dc2411a6c160316334d4d3a6a4a
gauravsh0812 <gauravs@arizona.edu>
2024-07-24 16:09:15 -0400

    update

---
+++ dataset/get_image_tensors.py
@@ -0,0 +1,42 @@
+import os
+from PIL import Image
+import torch
+from torchvision import transforms
+import yaml
+from box import Box
+import tqdm
+
+# reading config file
+with open("config.yaml") as f:
+    cfg = Box(yaml.safe_load(f))
+
+# Define a transformation to preprocess the images
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),  # Resize to 224x224
+    transforms.ToTensor(),  # Convert to tensor
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
+])
+
+def load_and_preprocess_image(image_path):
+    image = Image.open(image_path).convert("RGB")  # Open and convert to RGB
+    image_tensor = preprocess(image)  # Apply preprocessing
+    return image_tensor
+
+def save_tensor(tensor, file_path):
+    torch.save(tensor, file_path)
+
+# Define the directory containing images and the directory to save tensors
+image_dir = f"{cfg.dataset.path_to_data}/frames"
+tensor_dir = f"{cfg.dataset.path_to_data}/frame_tensors"
+os.makedirs(tensor_dir, exist_ok=True)
+
+# Process each image in the directory
+for image_name in tqdm.tqdm(os.listdir(image_dir)):
+    image_path = os.path.join(image_dir, image_name)
+    tensor = load_and_preprocess_image(image_path)
+    
+    # Save tensor with the same name as the image but with .pt extension
+    tensor_path = os.path.join(tensor_dir, f'{os.path.splitext(image_name)[0]}.pt')
+    save_tensor(tensor, tensor_path)
+
+print("Preprocessing and saving tensors completed.")
---
+++ dataset/preprocessing.py
@@ -0,0 +1,131 @@
+import os
+import yaml
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader, DistributedSampler
+from torch.utils.data import SequentialSampler
+from box import Box
+from transformers import RobertaTokenizer
+import torch.multiprocessing as mp
+
+mp.set_start_method('spawn', force=True)
+
+# reading config file
+with open("config.yaml") as f:
+    cfg = Box(yaml.safe_load(f))
+
+class Img2MML_dataset(Dataset):
+    def __init__(self, dataframe, ans_vocab):
+        self.dataframe = dataframe
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        img = self.dataframe.iloc[index, 0] 
+        lbl = self.dataframe.iloc[index,-1]
+        return img,lbl
+
+def data_loaders(batch_size):
+
+    print("creating dataloaders...")
+    
+    # two columns: [images, phases]
+    if not cfg.dataset.load_image_tensors:
+        df = pd.read_csv(f"{cfg.dataset.path_to_data}/extracted_frames_with_phases.csv")
+    else:
+        df = pd.read_csv(f"{cfg.dataset.path_to_data}/extracted_frames_tensors_with_phases.csv")
+
+    for _ in range(5):
+        df = df.sample(frac=1).reset_index(drop=True)
+
+    # split the image_num into train, test, validate
+    train_val_df, test_df = train_test_split(
+        df, test_size=0.1, random_state=42
+    )
+    train_df, val_df = train_test_split(
+        train_val_df, test_size=0.1, random_state=42
+    )
+    
+    print(f"saving dataset files to {cfg.dataset.path_to_data}/ folder...")
+    train_df.to_csv(f"{cfg.dataset.path_to_data}/train.csv", index=False)
+    test_df.to_csv(f"{cfg.dataset.path_to_data}/test.csv", index=False)
+    val_df.to_csv(f"{cfg.dataset.path_to_data}/val.csv", index=False)
+    
+    N = int(cfg.dataset.sample_size)
+    if N != -1:
+        train = train[:N]
+        test = test[:int(N*0.1)]
+        val = val[:int(N*0.1)]
+
+    print("training dataset size: ", len(train_df))
+    print("testing dataset size: ", len(test_df))
+    print("validation dataset size: ", len(val_df))
+
+    # initailizing class Img2MML_dataset: train dataloader
+    imml_train = Img2MML_dataset(train_df)
+    # creating dataloader
+    if cfg.general.ddp:
+        train_sampler = DistributedSampler(
+            dataset=imml_train,
+            num_replicas=cfg.general.world_size,
+            rank=cfg.general.rank,
+            shuffle=cfg.dataset.shuffle,
+        )
+        sampler = train_sampler
+        shuffle = False
+
+    else:
+        sampler = None
+        shuffle = cfg.dataset.shuffle
+        
+    train_dataloader = DataLoader(
+        imml_train,
+        batch_size=batch_size,
+        num_workers=cfg.dataset.num_workers,
+        shuffle=shuffle,
+        sampler=sampler,
+        pin_memory=cfg.dataset.pin_memory,
+    )
+
+    # initailizing class Img2MML_dataset: val dataloader
+    imml_val = Img2MML_dataset(val_df)
+    if cfg.general.ddp:
+        val_sampler = SequentialSampler(imml_val)
+        sampler = val_sampler
+        shuffle = False
+    else:
+        sampler = None
+        shuffle = cfg.dataset.shuffle
+
+    val_dataloader = DataLoader(
+        imml_val,
+        batch_size=batch_size,
+        num_workers=cfg.dataset.num_workers,
+        shuffle=shuffle,
+        sampler=sampler,
+        pin_memory=cfg.dataset.pin_memory,
+    )
+
+    # initailizing class Img2MML_dataset: test dataloader
+    imml_test = Img2MML_dataset(test_df)
+    if cfg.general.ddp:
+        test_sampler = SequentialSampler(imml_test)
+        sampler = test_sampler
+        shuffle = False
+    else:
+        sampler = None
+        shuffle = cfg.dataset.shuffle
+
+    test_dataloader = DataLoader(
+        imml_test,
+        batch_size=batch_size,
+        num_workers=cfg.dataset.num_workers,
+        shuffle=shuffle,
+        sampler=None,
+        pin_memory=cfg.dataset.pin_memory,
+    )
+
+    return (train_dataloader, 
+            test_dataloader, 
+            val_dataloader, )
