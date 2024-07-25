6c2220fe4c7c501d02932128d3faa9f6de59b0b8
gauravsh0812 <gauravs@arizona.edu>
2024-07-25 11:41:10 -0400

    update

--- src/models/clipmodel.py
+++ src/models/clipmodel.py
@@ -31,7 +31,7 @@ class ClipVisionEncoder(nn.Module):
             _hid.append(last_hidden_state.squeeze(0))
         
         # hidden: (B, L, 768)
-        return torch.stack(_hid).to(device)#, torch.stack(_pool).to(device)
+        return torch.stack(_hid).to(device)
     
 class Projector(nn.Module):
 
@@ -40,19 +40,18 @@ class Projector(nn.Module):
         self.final_lin1 = nn.Linear(768, 512)
         self.final_lin2 = nn.Linear(512, 64)
         self.final_lin3 = nn.Linear(64, num_classes)
-        self.norm1 = nn.BatchNorm1d(512)
-        self.norm2 = nn.BatchNorm1d(64)
-        self.nor3 = nn.BatchNorm1d(num_classes)
+        self.norm1 = nn.BatchNorm1d(50)
+        self.norm2 = nn.BatchNorm1d(50)
 
         self.pool = nn.AdaptiveAvgPool1d(1)
         self.gelu = nn.GELU()
 
     def forward(self, x):
-        x = self.gelu(self.norm(self.final_lin1(x)))
-        x = self.gelu(self.norm(self.final_lin2(x)))
-        x = self.pool(x)
-        x = torch.flatten(x, -2,-1)
-        x = self.gelu(self.norm(self.final_lin3(x)))
+        x = self.gelu(self.norm(self.final_lin1(x)))  
+        x = self.gelu(self.norm(self.final_lin2(x)))  # (B, 50, 64)
+        x = self.pool(x)    # (B, 1, 64)       
+        x = torch.flatten(x, -2,-1)   # (B, 64)
+        x = self.gelu(self.final_lin3(x))   # (B, num_classes)
 
         return x   # (B,num_classes)
 
@@ -60,9 +59,8 @@ class Cataract_Model(nn.Module):
 
     def __init__(self, num_classes):
 
-        super(Cataract_Model    , self).__init__()
+        super(Cataract_Model, self).__init__()
         self.clipenc = ClipVisionEncoder()
-        
         self.projector = Projector(num_classes)
         
         for param in self.clipenc.parameters():
@@ -74,10 +72,7 @@ class Cataract_Model(nn.Module):
             device,
         ):
 
-        encoded_imgs = self.clipenc(imgs, device)  # (B, L=w*h, dim)  
-        print("encoded_imagges shape: ", encoded_imgs.shape)
-        exit()
-
+        encoded_imgs = self.clipenc(imgs, device)  # (B, 50, dim)  
         projoutput = self.projector(encoded_imgs) # (B,num_classes)
         
         return projoutput
